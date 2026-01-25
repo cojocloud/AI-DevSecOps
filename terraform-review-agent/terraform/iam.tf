6473668d7bf4da608768a4e806c13414400153cb
cojocloud <cojocloudsolutions@gmail.com>
2026-01-25 15:43:46 -0500

    Update iam.tf

--- terraform-review-agent/terraform/iam.tf
+++ terraform-review-agent/terraform/iam.tf
@@ -56,3 +56,4 @@ resource "aws_iam_role_policy_attachment" "basic_logs" {
   role       = aws_iam_role.lambda_role.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
 }
+
