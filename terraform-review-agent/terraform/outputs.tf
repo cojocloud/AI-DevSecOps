2290642fe4cfb058ebddf59056f7bdfe81d744f9
cojocloud <cojocloudsolutions@gmail.com>
2026-01-26 09:29:56 -0500

    updated files

--- terraform-review-agent/terraform/outputs.tf
+++ terraform-review-agent/terraform/outputs.tf
@@ -3,8 +3,9 @@ output "alb_dns_name" {
   value       = aws_lb.app_alb.dns_name
 }
 
-
+/*
 output "acm_certificate_arn" {
-  description = "ARN of the ACM certificate for mario.cojocloudsolutions.com"
+  description = "ARN of the ACM certificate for mario.praveshsudha.com"
   value       = aws_acm_certificate.mario_cert.arn
 }
+*/
