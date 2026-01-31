e153332ac02b5ec1dc7238f0030e5872e55a041d
cojocloud <cojocloudsolutions@gmail.com>
2026-01-31 16:19:08 -0500

    updated alb.tf

--- terraform-review-agent/terraform/alb.tf
+++ terraform-review-agent/terraform/alb.tf
@@ -48,25 +48,3 @@ resource "aws_acm_certificate" "mario_cert" {
     create_before_destroy = true
   }
 }
-resource "aws_acm_certificate_validation" "mario_cert_validation" {
-  certificate_arn         = aws_acm_certificate.mario_cert.arn
-  validation_record_fqdns = [
-   for record in aws_route53_record.mario_cert_validation :
-    record.fqdn
-  ]
-}
-resource "aws_route53_record" "mario_cert_validation" {
-  for_each = {
-    for dvo in aws_acm_certificate.mario_cert.domain_validation_options : dvo.domain_name => {
-      name   = dvo.resource_record_name
-      type   = dvo.resource_record_type
-      record = dvo.resource_record_value
-    }
-  }
-
-  zone_id = var.route53_zone_id
-  name    = each.value.name
-  type    = each.value.type
-  ttl     = 60
-  records = [each.value.record]
-}
